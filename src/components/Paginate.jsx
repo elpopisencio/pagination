6d916ca9fccb469df21e26fd6bdf4e409733f94c
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-06-26 10:19:49 -0300

    added check for array

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -4,6 +4,12 @@ import PaginationNav from './PaginationNav';
 const Paginate = ({ children, pageSize }) => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const pagesAmount = Math.ceil(children.length / pageSize);
+	if (children.constructor !== Array) {
+		console.error(
+			'The children of the pagination component must be an Array'
+		);
+		return <div />;
+	}
 	const paginatedChildren = children.slice(
 		(currentPage - 1) * pageSize,
 		currentPage * pageSize
