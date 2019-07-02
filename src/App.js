fda824f0730d9dbe1dada24fac773b83f31e0071
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-07-02 10:25:29 -0300

    added links

--- src/App.js
+++ src/App.js
@@ -22,7 +22,29 @@ const App = () => {
 			<div className="hero">
 				<div className="hero-body">
 					<h1 className="title is-1">Frontend Pagination.</h1>
-					<h2 className="subtitle is-4">Made with React.</h2>
+					<h2 className="subtitle is-4">
+						<div className="level">
+							<div className="level-left">Made with React.</div>
+							<div className="level-right">
+								<a
+									style={{ color: 'inherit' }}
+									href="https://elpopisencio.github.io"
+								>
+									<span class="icon is-large">
+										<i class="fa fa-home"></i>
+									</span>
+								</a>
+								<a
+									style={{ color: 'inherit' }}
+									href="https://github.com/elpopisencio/pagination"
+								>
+									<span class="icon is-large">
+										<i class="fab fa-github"></i>
+									</span>
+								</a>
+							</div>
+						</div>
+					</h2>
 				</div>
 			</div>
 			<img className="project-image" src={book} alt="" />
